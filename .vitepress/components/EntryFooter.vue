25c7408127ceb2ef49aaf23b121de902e58d54f6
1Step621 <onestep621@gmail.com>
2024-03-20 17:43:34 +0900

    fix&enhance: next/prev entry

--- .vitepress/components/EntryFooter.vue
+++ .vitepress/components/EntryFooter.vue
@@ -1,35 +1,70 @@
 <script lang="ts" setup>
-import { computed, ComputedRef } from 'vue';
+import { computed } from "vue";
 import { ContentData, useRoute, withBase } from 'vitepress';
 // @ts-expect-error
 import { data } from '../scripts/entries.data'
 import { getTitle } from '../scripts/get-from-entry';
-const entries: ComputedRef<ContentData[]> = computed(() => [...data]);
-const { path } = useRoute();
-const index = computed(() => entries.value.findIndex((entry: ContentData) => withBase(entry.url) === path));
-const prev = computed(() => entries.value[index.value - 1]);
-const next = computed(() => entries.value[index.value + 1]);
+const entries: ContentData[] = [...data];
+const route = useRoute();
+const index = computed(() => entries.findIndex((entry) => withBase(entry.url) === route.path));
+const prev = computed(() => entries[index.value - 1]);
+const next = computed(() => entries[index.value + 1]);
 </script>
 
 <template>
   <div class="pageFooter">
-    <a :href="withBase(prev.url)" v-if="prev" class="prev">< {{ getTitle(prev) }}</a>
-    <a :href="withBase(next.url)" v-if="next" class="next">{{ getTitle(next) }} ></a>
+    <a class="VPLink link pager-link prev" :href="withBase(prev.url)" v-if="prev"> {{ getTitle(prev) }}</a>
+    <a class="VPLink link pager-link next" :href="withBase(next.url)" v-if="next">{{ getTitle(next) }} </a>
   </div>
 </template>
 
 <style>
 .pageFooter {
   position: relative;
-  gap: 50px;
-  margin-top: 50px;
 }
+
+.pageFooter * {
+  display: block;
+  position: absolute;
+  border: 1px solid var(--vp-c-divider);
+  border-radius: 8px;
+  padding: 11px 16px 13px;
+  transition: border-color .25s, color .25s, transform .25s;
+  width: 45%;
+  line-height: 20px;
+  font-size: 14px;
+  font-weight: 500;
+  color: var(--vp-c-brand-1);
+}
+.pageFooter *:hover {
+  border-color: var(--vp-c-brand-1);
+}
+
 .pageFooter .prev {
-  position: absolute;
   left: 0;
+  text-align: left;
 }
+.pageFooter .prev:hover {
+  transform: translate(-5px, 0);
+}
+.pageFooter .prev::before {
+  display: block;
+  color: var(--vp-c-text-2);
+  font-size: 12px;
+  content: "Previous page";
+}
+
 .pageFooter .next {
-  position: absolute;
   right: 0;
+  text-align: right;
+}
+.pageFooter .next:hover {
+  transform: translate(5px, 0);
+}
+.pageFooter .next::before {
+  display: block;
+  color: var(--vp-c-text-2);
+  font-size: 12px;
+  content: "Next page";
 }
 </style>
